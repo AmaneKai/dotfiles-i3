9b3e126834ccd984c9732fc8925412fcd485c178
Amane Kai / 雨音カイ <carlosranara0@gmail.com>
2026-06-24 11:06:24 +0800

    feat: add display switching

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# 1. Kill any existing polybar instances
 killall -q polybar
+while pgrep -u "$UID" -x polybar >/dev/null; do sleep 1; done
 
-# 2. Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+CFG="/home/amane/.dotfiles-i3/polybar/.config/polybar/config.ini"
 
-# 3. Detect if HDMI-1 is active (Look for a resolution like 1920x1080)
-if xrandr | grep "HDMI-1 connected" | grep -q "[0-9]x[0-9]"; then
-    # HDMI is plugged in and active
-    polybar monitor &
+if xrandr --query | grep -q "HDMI-A-0 connected [0-9]"; then
+    MONITOR=HDMI-A-0 polybar --config="$CFG" monitor &
 else
-    # HDMI is gone or inactive, use laptop bar
-    polybar laptop &
+    MONITOR=eDP polybar --config="$CFG" laptop &
 fi
